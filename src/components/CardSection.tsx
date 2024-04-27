2589bc0e36372f1915101c95a3c11c15c3327d2b
L3onardo <suchyleon4@gmail.com>
2024-04-27 21:22:43 +0200

    neco

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 export default function CardSection(){
     return(
         <>
-            <section className="flex flex-row px-20 justify-around">
+            <section className="flex flex-row px-20 justify-around ">
                 <Card heading="Lorem Ipsum" paragraph="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Aliquam in lorem sit amet leo accumsan lacinia. Aliquam erat volutpat. Mauris metus."/>
                 <Card heading="Lorem Ipsum" paragraph="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc auctor. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Aliquam in lorem sit amet leo accumsan lacinia. Aliquam erat volutpat. Mauris metus."/>
             </section>
