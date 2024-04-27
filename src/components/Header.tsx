cf308378dc276c4b9422a8ed962ce58b2bb0268c
L3onardo <suchyleon4@gmail.com>
2024-04-27 18:29:41 +0200

    theme-switcher inited, edited header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
-
+import ModeToggle from "./ModeToggle"
 
 export default function Header(){
     return(
         <>
-            <h1>
-                Hello world
-            </h1>
-
+            <header className="flex flex-row align-center justify-center w-full gap-10 m-6">
+                    <h1>
+                        Ahoj vojtisku, toto je header našeho nextjs projektu s ShadCN theme switchem
+                    </h1>
+                <br />
+                    <ModeToggle />
+            </header>
         </>
     )
 
