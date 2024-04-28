fd52ecf0de12d6c76dc8606bede86c42a973ffe7
DrZet01 <159666464+DrZet01@users.noreply.github.com>
2024-04-28 19:08:59 +0200

    Řekni mi, proč ta sračka nefunguje a svítí červeně <3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-
+'use client'
 
 import ModeToggle from "./ModeToggle";
 import ScrollBar from "./ScrollBar";
 import AnimatedTextWord from "./AnimatedTextWord";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Modal from "./modal";
+import { useState } from "react";
 import {
   Menubar,
   MenubarCheckboxItem,
@@ -26,6 +28,12 @@ import "./waveShadow.css";
 
 
 export default function Header(){
+
+    const[mujUcetModalIsOpen, setMujUcetModalIsOpen] = useState(false);
+
+    const openMujUcetModal = () => setMujUcetModalIsOpen(true);
+    const closeMujUcetModal = () => setMujUcetModalIsOpen(false);
+    
     return(
         <> 
             <ScrollBar />
@@ -41,7 +49,7 @@ export default function Header(){
                         <MenubarTrigger>Menu</MenubarTrigger>
                         <MenubarContent>
                             <a href="#header"><MenubarItem>Domů</MenubarItem></a>
-                            <MenubarItem disabled>Můj účet</MenubarItem>
+                            <MenubarItem onClick={openMujUcetModal}>Můj účet</MenubarItem>
                             <MenubarItem>Psát</MenubarItem>
                         </MenubarContent>
                     </MenubarMenu>
@@ -55,6 +63,8 @@ export default function Header(){
                     <div className="font-light mt-4 text-xl sticky"><AnimatedTextWord text="Created by: Leon Suchý, Vojtěch Reiss, Rostislav Kupstov"/></div>
             </header>
             <div className="headerWave"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path className="fill-cyan-700 dark:fill-cyan-800" fill-opacity="1" d="M0,256L60,240C120,224,240,192,360,181.3C480,171,600,181,720,165.3C840,149,960,107,1080,112C1200,117,1320,171,1380,197.3L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg></div>
+
+            {mujUcetModalIsOpen && (<Modal />)}
         </>
     )
 
