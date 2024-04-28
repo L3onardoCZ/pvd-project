9de410de01ce2dd7e99e23e2ee857e3ad2c46b6c
L3onardo <suchyleon4@gmail.com>
2024-04-28 20:50:11 +0200

    hh

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,34 @@
 import AccountTab from "./AccountTab"
+import Leaderboard from "./Leaderboard"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+  } from "@/components/ui/avatar"
+   
+
 
 export default function Dashboard(){
     return(
         <>
-            <AccountTab/>
-        
+            <div className="w-full flex justify-center gap-8">
+                <p>Placement</p>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    2
+                </h1>
+                <p>Top WPM</p>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    350
+                </h1>
+                <p>Excercises</p>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    24
+                </h1>
+            </div>
+            <div className="w-full flex justify-center gap-8 mt-8">
+                <AccountTab/>
+                <Leaderboard/>
+            </div>
         </>
     )
 }
