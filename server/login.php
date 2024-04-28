5d25277596abb2dd7a62b74772d42588ac7e74f8
DrZet01 <159666464+DrZet01@users.noreply.github.com>
2024-04-28 12:51:26 +0200

    Přidání hlaviček do login.php

--- server/login.php
+++ server/login.php
@@ -1,4 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Content-type: application/json');
+header('Content-Type: text/html; charset=utf-8');
 session_start();
 ?>
 
