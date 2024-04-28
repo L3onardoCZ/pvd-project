e53d930eb1b15862b2a8d40b1a03ff8965e17f6b
L3onardo <suchyleon4@gmail.com>
2024-04-28 20:27:13 +0200

    Smallfooter

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,10 +1,12 @@
 import SmallHeader from "@/components/SmallHeader";
+import SmallFooter from "@/components/SmallFooter";
 
 
 export default function account(){
     return(
         <>
             <SmallHeader/>
+            <SmallFooter/>
         </>
     )
 }
