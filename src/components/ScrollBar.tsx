9e8b46f934d73b8b795f9988d5651ce5b0101c84
L3onardo <suchyleon4@gmail.com>
2024-04-28 15:10:28 +0200

    scrollbar

--- src/components/ScrollBar.tsx
+++ src/components/ScrollBar.tsx
@@ -1,9 +1,21 @@
-import { motion } from "framer-motion";
+"use client"
+import "./progressbar.css"
 
 
-export default function ScrollBar(){
-    return(
-        <>
-        </>
-    )
+import { motion, useScroll, useSpring } from "framer-motion";
+
+export default function ScrollBar() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 400,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+  return (
+    <>
+      <motion.div className="progress-bar z-40" style={{ scaleX }} />
+
+    </>
+  );
 }
