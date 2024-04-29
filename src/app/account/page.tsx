aab5b5b28d4380d9f094285c137501e863376b0e
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-04-29 08:39:40 +0200

    Update page.tsx

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,7 @@
 import SmallHeader from "@/components/SmallHeader";
 import SmallFooter from "@/components/SmallFooter";
 import Dashboard from "@/components/Dashboard";
+import React from 'react'; 
 
 export default function account(){
     return(
