2c8d55eb2f217ffd717fe1a721bdb046f742c18b
L3onardo <suchyleon4@gmail.com>
2024-05-01 09:44:46 +0200

    final bug fix hydratation error

--- src/components/BackgroundLetters.tsx
+++ src/components/BackgroundLetters.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import "./backgroundletters.css";
@@ -16,50 +16,52 @@ function getRandomNumberInRange(min: number, max: number): number {
 }
 
 function BackgroundLetters(): JSX.Element {
-
   const { scrollY } = useScroll();
 
-    const rotate = useTransform(scrollY, [0, 1000], [0, 100]);
-    const opacity = useTransform(scrollY, [0, 1000], [0.1, 0.4]);
-    const translateY = useTransform(scrollY, [0, 1000], [0, -300])
-    const translateX = useTransform(scrollY, [0, 1000], [0, 20])
-
-  
+  const rotate = useTransform(scrollY, [0, 1000], [0, 100]);
+  const opacity = useTransform(scrollY, [0, 1000], [0.1, 0.4]);
+  const translateY = useTransform(scrollY, [0, 1000], [0, -300]);
+  const translateX = useTransform(scrollY, [0, 1000], [0, 20]);
 
   const [ref, inView] = useInView({
     triggerOnce: true,
     threshold: 1,
   });
 
+  const [randomLetters, setRandomLetters] = useState<string[]>([]);
+
+  useEffect(() => {
+    const generatedLetters = Array.from({ length: 80 }, () => getRandomLetter());
+    setRandomLetters(generatedLetters);
+  }, []); // Použijeme useEffect pro generování písmen pouze při prvotním načtení komponenty
+
   return (
     <div className="background-letters">
-      {Array.from({ length:80 }).map((_, index) => {
-        const randomScale = getRandomNumberInRange(0.4, 1); 
-        const randomYOffset = getRandomNumberInRange(-50, 50); 
-        const randomXOffset = getRandomNumberInRange(-50, 50); 
-        
+      {randomLetters.map((letter, index) => {
+        const randomScale = getRandomNumberInRange(0.4, 1);
+        const randomYOffset = getRandomNumberInRange(-50, 50);
+        const randomXOffset = getRandomNumberInRange(-50, 50);
 
         return (
           <motion.p
             key={index}
             style={{
-              fontSize: `${randomScale * 3}rem`, 
-              top: `${80 + randomYOffset}vh`, 
-              left: `${50 + randomXOffset}vw`, 
-              rotate: rotate, 
-              opacity: opacity, 
+              fontSize: `${randomScale * 3}rem`,
+              top: `${80 + randomYOffset}vh`,
+              left: `${50 + randomXOffset}vw`,
+              rotate: rotate,
+              opacity: opacity,
               translateY: translateY,
               translateX: translateX,
             }}
             ref={ref}
-            initial={{y: 200, opacity: 0 }}
-            animate={{y: inView ? 0 : 200, opacity: inView ? 0.2 : 0 }}
+            initial={{ y: 200, opacity: 0 }}
+            animate={{ y: inView ? 0 : 200, opacity: inView ? 0.2 : 0 }}
             transition={{
-              y: { duration: 30, repeat: Infinity, repeatType: "reverse" }, // Nekonečná animace s reverzním opakováním
+              y: { duration: 30, repeat: Infinity, repeatType: "reverse" },
             }}
-
           >
-            {getRandomLetter()}
+            {letter}
           </motion.p>
         );
       })}
