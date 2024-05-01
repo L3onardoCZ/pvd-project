0b77ad463d9ac32689fe36a242afb7df7b1882e7
L3onardo <suchyleon4@gmail.com>
2024-05-01 09:25:47 +0200

    bugfix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ export default function Header(){
                     </div>
                     <div className="font-light mt-4 text-xl sticky"><AnimatedTextWord text="Created by: Leon Suchý, Vojtěch Reiss, Rostislav Kupstov"/></div>
             </header>
-            <div className="headerWave z-50"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path className="fill-cyan-700 dark:fill-cyan-800" fill-opacity="1" d="M0,256L60,240C120,224,240,192,360,181.3C480,171,600,181,720,165.3C840,149,960,107,1080,112C1200,117,1320,171,1380,197.3L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg></div>
+            <div className="headerWave z-50"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path className="fill-cyan-700 dark:fill-cyan-800" d="M0,256L60,240C120,224,240,192,360,181.3C480,171,600,181,720,165.3C840,149,960,107,1080,112C1200,117,1320,171,1380,197.3L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg></div>
 
 
         </>
