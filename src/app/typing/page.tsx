91e9f740cc1df54c47a250d0ec825862a28e8753
L3onardo <suchyleon4@gmail.com>
2024-05-05 12:04:29 +0200

    update

--- src/app/typing/page.tsx
+++ src/app/typing/page.tsx
@@ -2,12 +2,14 @@ import SmallHeader from "@/components/SmallHeader"
 import SmallFooter from "@/components/SmallFooter"
 import TypingSection from "@/components/TypingSection"
 
+
 export default function typing(){
     return(
         <>
             <SmallHeader/>
             <TypingSection/>
             <SmallFooter/>
+
         </>
     )
 }
