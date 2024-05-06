fb85db4d8ceb3f6e8fc30416521ecc5d3b4a9dde
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-05-06 14:52:48 +0200

    vrafceno do puvodniho stavu, seru na to

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -2,17 +2,15 @@ import SmallHeader from "@/components/SmallHeader";
 import SmallFooter from "@/components/SmallFooter";
 import Dashboard from "@/components/Dashboard";
 import React from 'react'; 
-import IsLoggedIn from "@/components/IsLoggedIn";
+
 
 export default function Account(){
 
     return(
         <>
-            <IsLoggedIn>
-                <SmallHeader/>
-                <Dashboard/>
-                <SmallFooter/>
-            </IsLoggedIn>
+            <SmallHeader/>
+            <Dashboard/>
+            <SmallFooter/>
         </>
     )
 }
