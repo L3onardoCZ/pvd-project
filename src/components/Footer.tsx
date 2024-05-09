0e883e0007668db86e1621248c01e8338c544d51
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-05-09 10:46:11 +0200

    copyright symbols footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,9 @@ import "./waveShadow.css"
 export default function Footer(){
     return(
         <>
+            <div className="flex w-full justify-center">
+                <h3 className="absolute z-40 mt-[250px]">© 2024</h3>
+            </div>
             <footer className="footerWave"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path className="fill-cyan-700 dark:fill-cyan-800" d="M0,224L60,229.3C120,235,240,245,360,234.7C480,224,600,192,720,186.7C840,181,960,203,1080,224C1200,245,1320,267,1380,277.3L1440,288L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg></footer>
         </>
     )
--- src/components/SmallFooter.tsx
+++ src/components/SmallFooter.tsx
@@ -1,7 +1,14 @@
 export default function SmallFooter(){
     return(
         <>
-            <footer className="footerWave bottom-0 w-full relative"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path className="fill-cyan-700 dark:fill-cyan-800" fill-opacity="1" d="M0,224L48,234.7C96,245,192,267,288,261.3C384,256,480,224,576,197.3C672,171,768,149,864,170.7C960,192,1056,256,1152,256C1248,256,1344,192,1392,160L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg></footer>
+
+                <div>
+                    <div className="flex w-full justify-center">
+                        <h3 className="absolute z-40 mt-[250px]">© 2024</h3>
+                    </div>
+                    <footer className="footerWave bottom-0 w-full relative"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path className="fill-cyan-700 dark:fill-cyan-800" fill-opacity="1" d="M0,224L48,234.7C96,245,192,267,288,261.3C384,256,480,224,576,197.3C672,171,768,149,864,170.7C960,192,1056,256,1152,256C1248,256,1344,192,1392,160L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg></footer>
+                </div>
+
         </>
     )
 }
