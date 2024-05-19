3c0e81790849566414ed1c775421443d000a1b1f
DrZet01 <159666464+DrZet01@users.noreply.github.com>
2024-05-19 10:55:03 +0200

    images ty kluku

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     output: 'export',
     //basePath: '/pvd-project',
+
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'aceternity.com',
+          port: '',
+          pathname: '/images/**',
+        },
+      ],
+    },
   }
--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    //basePath: '/pvd-project',
-};
-
-export default nextConfig;
