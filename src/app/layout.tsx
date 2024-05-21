78cf3b219df2844a544864943333b66eb0a67fee
L3onardo <suchyleon4@gmail.com>
2024-05-21 15:14:25 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content="PVD-project je webová aplikace, která slouží k procvičování Vaší klávesnicové gramotnosti." />
       <meta name="keywords" content="psaní všemi deseti, psani vsemi deseti, PVD, PVD-PROJECT, pvd-project, all ten fingers" />
+      <link rel="icon" type="image/x-icon" href="./favicon.png"></link>
       </head>
       <body className={poppins.className}>
 
